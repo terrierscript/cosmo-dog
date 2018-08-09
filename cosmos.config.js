8c76525650062168ed178942f43a7760a271a862
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-08-10 08:49:53 +0900

    add

--- cosmos.config.js
+++ cosmos.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  webpack(config) {
-    return {
-      ...config,
-      resolve: {
-        extensions: [".ts", ".tsx", ".css", ".svg", ".js", ".jsx"]
-      }
-    };
-  }
+  webpackConfigPath: "node_modules/react-scripts-ts/config/webpack.config.dev"
+
+  // webpack(config) {
+  //   return {
+  //     ...config,
+  //     resolve: {
+  //       extensions: [".ts", ".tsx", ".css", ".svg", ".js", ".jsx"]
+  //     }
+  //   };
+  // }
 };
