f89cc495b2ace2d35d91acfa37fbdd74cc64ffb9
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-08-11 11:44:59 +0900

    add

--- cosmos.config.js
+++ cosmos.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-  webpackConfigPath: "node_modules/react-scripts-ts/config/webpack.config.dev"
+  // webpackConfigPath: "node_modules/react-scripts-ts/config/webpack.config.dev"
 
-  // webpack(config) {
-  //   return {
-  //     ...config,
-  //     resolve: {
-  //       extensions: [".ts", ".tsx", ".css", ".svg", ".js", ".jsx"]
-  //     }
-  //   };
-  // }
+  webpack(config) {
+    return {
+      ...config,
+      resolve: {
+        extensions: [".ts", ".tsx", ".css", ".svg", ".js", ".jsx"]
+      },
+      module: {
+        rules: [
+          // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+          { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+      }
+    };
+  }
 };
