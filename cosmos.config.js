2c325815ced3bbba38dffacf7fd6b4d1ece991b4
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-08-11 11:48:25 +0900

    add

--- cosmos.config.js
+++ cosmos.config.js
@@ -10,7 +10,11 @@ module.exports = {
       module: {
         rules: [
           // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-          { test: /\.tsx?$/, loader: "ts-loader" }
+          { test: /\.tsx?$/, loader: "ts-loader" },
+          {
+            test: /\.(jpg|png|gif)$/,
+            loader: ["url-loader"]
+          }
         ]
       }
     };
