dfbb9f798ee36c9916382da0620bd46e1f241595
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-08-12 10:11:59 +0900

    soler panel

--- src/calculator/Calculator.tsx
+++ src/calculator/Calculator.tsx
@@ -47,6 +47,31 @@ const Input = styled.input`
 
 const NumberButton = GrayButton;
 
+const Panel = styled.div`
+  height: 40px;
+  width: 32px;
+  margin-left: 1px;
+  background: rgb(78, 13, 13);
+  background: linear-gradient(
+    5deg,
+    rgba(78, 13, 13, 1) 0%,
+    rgba(145, 51, 51, 1) 47%,
+    rgba(102, 10, 10, 1) 56%,
+    rgba(186, 83, 83, 1) 59%,
+    rgba(125, 41, 41, 1) 61%
+  );
+  display: inline-block;
+`;
+const SolarPanel = () => {
+  return (
+    <Area area="so">
+      <Panel />
+      <Panel />
+      <Panel />
+      <Panel />
+    </Area>
+  );
+};
 const Numbers = () => {
   return (
     <>
@@ -123,6 +148,7 @@ export const Calcurator = () => {
         <Area area="ii">
           <Input />
         </Area>
+        <SolarPanel />
         <Numbers />
         <Cal />
       </Grid>
