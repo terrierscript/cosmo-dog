41e3a1c1ced953ebc01dbf65a77143a76569ba97
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-08-19 20:17:05 +0900

    add

--- src/daily-ui/02_checkout/Checkout.tsx
+++ src/daily-ui/02_checkout/Checkout.tsx
@@ -1,23 +1,57 @@
 import React, { SFC, Children, ReactNode, createContext } from "react";
 
-class CardInputSanitized extends React.Component<
-  { number: string; children: (string) => ReactNode },
-  { saniizied: string }
-> {
-  render() {
-    return (
-      <div>
-        <input type="tel" />
-      </div>
-    );
-  }
-}
-export const Checkout = ({ name }) => {
-  return <div>Please input your card {name}</div>;
+import InputMask from "react-input-mask";
+import styled, { css } from "styled-components";
+import { areaStyle, Area } from "components/grid/Grid";
+const Input = styled.input`
+  /* line-height: em; */
+  padding: 0.5em;
+  font-size: 1.2em;
+  color: hsl(0, 0%, 45%);
+`;
+
+export const CardInput = (props) => {
+  return (
+    <InputMask mask="9999 9999 9999 9999" maskChar={null} value={props.number}>
+      {(inputProps) => <Input {...inputProps} />}
+    </InputMask>
+  );
 };
 
-type Typ = {
-  state?: { value: number };
-  actions?: { setValue: (value: number) => void };
+const template = `
+  "desc desc    preview"
+  "first last   preview"
+  "number numbe previewr"
+  
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template: ${template};
+`;
+
+const Title = styled.div``;
+export const Checkout = ({ name }) => {
+  return (
+    <Grid>
+      <Area area="desc">Please input your card</Area>
+      <Area area="first">
+        <label>
+          <Title>First Name</Title>
+          <Input />
+        </label>
+      </Area>
+      <Area area="last">
+        <label>
+          <Title>Last Name</Title>
+          <Input />
+        </label>
+      </Area>
+      <Area area="number">
+        <Title>Card Number</Title>
+        <CardInput />
+      </Area>
+      <Area area="preview" />
+    </Grid>
+  );
 };
-export const { Provider, Consumer } = createContext<Typ>({});
