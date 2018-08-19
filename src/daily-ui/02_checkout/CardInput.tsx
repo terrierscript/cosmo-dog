127f77b357db23f5bf754f26fba31ce456402488
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-08-19 15:57:43 +0900

    add

--- src/daily-ui/02_checkout/CardInput.tsx
+++ src/daily-ui/02_checkout/CardInput.tsx
@@ -2,12 +2,21 @@ import { ReactNode } from "react";
 import React from "react";
 import styled from "styled-components";
 
+import InputMask from "react-input-mask";
 const Input = styled.input`
   /* line-height: em; */
   padding: 0.5em;
   font-size: 1.2em;
   color: hsl(0, 0%, 45%);
 `;
+
+export const CardInput = () => {
+  return (
+    <InputMask mask="9999 9999 9999 9999" maskChar={null}>
+      {inputProps => <Input {...inputProps} />}
+    </InputMask>
+  );
+};
 
 const chunk = (input, size) => {
   return input.reduce((arr, item, idx) => {
@@ -16,8 +25,7 @@ const chunk = (input, size) => {
       : [...arr.slice(0, -1), [...arr.slice(-1)[0], item]];
   }, []);
 };
-
-export class CardInput extends React.Component<
+export class CardInputOld extends React.Component<
   { number: string; children: (string) => ReactNode },
   { sanitized: string }
 > {
@@ -26,10 +34,11 @@ export class CardInput extends React.Component<
     const num = chunk(number.split(""), 4)
       .map(item => item.join(""))
       .join(" ");
+    console.log(num);
     this.setState({ sanitized: num });
   }
   handleChange = e => {
-    const unchunked =  e.target.value.split(" ").join("")
+    const unchunked = e.target.value.split(" ").join("");
     this.sanitizeNumber(unchunked);
   };
   componentDidMount() {
@@ -37,8 +46,6 @@ export class CardInput extends React.Component<
   }
   render() {
     const { sanitized } = this.state;
-    return (
-        <Input type="tel" value={sanitized} onChange={this.handleChange} />
-    );
+    return <Input type="tel" value={sanitized} onChange={this.handleChange} />;
   }
 }
