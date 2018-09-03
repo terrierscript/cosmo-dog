51595ef890894584a0e5ed2903c129576edec960
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-09-03 23:21:33 +0900

    borders

--- src/components/grid/__fixtures__/Border.fixture.tsx
+++ src/components/grid/__fixtures__/Border.fixture.tsx
@@ -7,5 +7,22 @@ export default [
       top: true,
       children: "Top"
     }
+  },
+  {
+    component: Border,
+    props: {
+      top: true,
+      left: true,
+      children: "Top Left"
+    }
+  },
+  {
+    component: Border,
+    props: {
+      right: true,
+      bottom: true,
+      width: 2,
+      children: "Bottom Rigth"
+    }
   }
 ];
