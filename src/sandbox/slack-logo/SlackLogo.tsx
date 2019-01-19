36dcfbe68d4fdbdf511e4d2da0afdeadbd1dc6cb
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-01-19 16:04:27 +0900

    add

--- src/sandbox/slack-logo/SlackLogo.tsx
+++ src/sandbox/slack-logo/SlackLogo.tsx
@@ -11,16 +11,20 @@ const template = `
 const Grid = styled.div`
   display: grid;
   grid-template-areas: ${template};
-  grid-gap: 2px;
-  grid-template-columns: repeat(4, 10px);
-  grid-template-rows: repeat(4, 10px);
+  grid-gap: 4px;
+  grid-template-columns: repeat(4, 20px);
+  grid-template-rows: repeat(4, 20px);
 `;
 
 const CssVars = styled.div`
-  --b: #29c5f0;
   --g: #23b87b;
+  --b: #29c5f0;
   --r: #e31558;
   --y: #ecb208;
+  --rad-g: 2em 2em 2em 0;
+  --rad-b: 2em 2em 0 2em;
+  --rad-r: 2em 0 2em 2em;
+  --rad-y: 0 2em 2em 2em;
 `
 
 const Box = styled.div<{
@@ -31,24 +35,10 @@ const Box = styled.div<{
   height: 100%;
   background: ${ ({color}) => `var(--${color})` };
   grid-area: ${ ({color, size}) => `${color}${size}`};
-  border-radius: ${({color, size}) => borderRadiusMap(color, size)};
+  border-radius: ${({color}) => `var(--rad-${color},2em)` };
 `
-const borderRadiusMap = (color, size) => {
-  const r = "2em";
-  if(size != 1){
-    return r
-  }
-  switch(color){
-    case "g":
-      return `${r} ${r} ${r} 0`
-    case "b":
-      return `${r} ${r} 0 ${r}`
-    case "r":
-      return `${r} 0 ${r} ${r}`
-    case "y":
-      return `0 ${r} ${r} ${r}`
-  }
-}
+
+
 export const SlackLogo = () => {
   return <CssVars>
     <Grid>{
