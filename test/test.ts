48d7b9849fbdd30b4879427ebb750bde9205dbae
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-02-17 10:23:52 +0900

    add

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,6 @@
 import runTests from "react-cosmos-telescope";
 
-console.log(require.resolve("./cosmos.config.js"));
+console.log(require.resolve("../cosmos.config.js"));
 runTests({
-  cosmosConfigPath: require.resolve("./cosmos.config.js")
+  cosmosConfigPath: require.resolve("../cosmos.config.next.js")
 });
